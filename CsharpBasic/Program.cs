fdb6ed411a28a23a1515b9f1a6617538e1d93269
ohyeeune <s2018s33@e-mirim.hs.kr>
2020-04-28 14:21:50 +0900

    C# 기본 연습

--- CsharpBasic/Program.cs
+++ CsharpBasic/Program.cs
@@ -4,12 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CsharpBasic
+namespace CSBasic
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //273;
+            var name = "윤" + "인" + "성";
+            Console.Write("Hellp C#");
+            int add = 2;
+            int Alpha;
+            Console.WriteLine("Hello World");
+            Console.Write("Hello World");
+            Console.Write("Hello C#");
+
+            Console.Write(52);
+            Console.WriteLine(7 % 3);
+
+            Console.Write('A');
+            Console.Write("A");
+
+            Console.WriteLine("한빛\t아카데미");
+            Console.WriteLine("한빛\n아카데미");
+            Console.WriteLine("\"\"\"");
+            Console.WriteLine("가" + "나" + "다");
+
+            Console.WriteLine("안녕"[100]);
+
+            Console.WriteLine(true);
+            Console.WriteLine(false);
         }
     }
 }
